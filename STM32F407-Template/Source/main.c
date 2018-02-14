21cfb49e395be2e9b317ad5fbff4dbb932ca0d90
kowalski100 <ijaz.a100@yahoo.com>
2018-02-14 21:00:25 +0500

    Project Template

---
+++ STM32F407-Template/Source/main.c
@@ -0,0 +1,27 @@
+/*
+ * @author:         Ijaz Ahmad
+ *
+ * @Warrenty:       void
+ *
+ * @description:    STM32F407-Discovery Template Project on HAL Libraries 
+ *
+ */
+
+/*
+ * @WebLink: 
+ */
+
+#include "stm32f4xx_hal.h"
+
+int main () {
+    
+    /*
+        initialize HAL Library. This step is mandatory
+        for HAL libraries to work properly. It configures
+        SysTick etc. for its internal configurations.
+    */
+    HAL_Init();
+    
+    return 0;
+}
+
